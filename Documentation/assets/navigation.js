79c36ad71d386d7a8634ef2661924845b51b0d32
Nikola Hristov <nikola@nikolahristov.tech>
2023-12-17 08:00:53 +0200

    squash!

--- Documentation/assets/navigation.js
+++ Documentation/assets/navigation.js
@@ -1,1 +1,2 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAACouuVipJrShRslJKSU1LLM0pUdJRKkgsyVCyUnIrzUsuyczP04fK6GWU5OYo6ShlZ+alKFmZmdTGAgCqSXYoPQAAAA=="
+window.navigationData =
+	"data:application/octet-stream;base64,H4sIAAAAAAAACouuVipJrShRslJKSU1LLM0pUdJRKkgsyVCyUnIrzUsuyczP04fK6GWU5OYo6ShlZ+alKFmZmdTGAgCqSXYoPQAAAA==";
